401bff0e484d90d8ab0158b0753e60c053ebc0b5
Fırat Özkan <foskamzom@hotmail.com>
2021-07-05 00:43:24 +0300

    Güncel versiyon. / Updated version.

---
+++ Clock_Config.h
@@ -0,0 +1,8 @@
+#ifndef CLOCK_CONFIG_H
+
+#define CLOCK_CONFIG_H
+
+void Clock_Config(void);
+
+
+#endif
--- spi_accelerometer_register_level/Clock_Config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLOCK_CONFIG_H
-
-#define CLOCK_CONFIG_H
-
-void Clock_Config(void);
-
-
-#endif
